f60853a31ed2eda8f0a353f98d61f405ca4943c1
mb1AtGithub <Manisha.bapat16@gmail.com>
2025-12-14 14:33:41 +0000

    postlua

---
+++ post.lua
@@ -0,0 +1,3 @@
+wrk.method = "POST"
+wrk.headers["Content-Type"] = "application/json"
+wrk.body = '{"sno":87.0,"age":46.0,"gender":0.0,"cp":1.0,"trestbps":101.0,"chol":197.0,"fbs":1.0,"restecg":1.0,"thalach":156.0,"exang":0.0,"oldpeak":0.0,"slope":2.0,"ca":0.0,"thal":3.0}'
